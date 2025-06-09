e558795abec8ebf43d99a5f6879932186e6124c8
leolmeta <lolmetab@gmail.com>
2025-06-09 01:25:11 -0400

    added appropriate structure and syntax to files

--- data/fetch.py
+++ data/fetch.py
@@ -3,25 +3,28 @@ import sys
 import os
 import pandas as pd
 
-# get ticker symbol list
+def main():
+    # get ticker symbol list
 
-sys.path.append(os.path.abspath('..'))
-from config import ticker_symbols
+    sys.path.append(os.path.abspath('..'))
+    from config import ticker_symbols
 
-# fetch requested ticker data
+    # fetch requested ticker data
 
-data = yf.download(ticker_symbols, period='max', interval='1d')
-os.makedirs('by_stock', exist_ok=True)
+    data = yf.download(ticker_symbols, period='max', interval='1d')
+    os.makedirs('by_stock', exist_ok=True)
 
-# split each ticker into it's own file
+    # split each ticker into it's own file
 
-if isinstance(data.columns, pd.MultiIndex):
-    for ticker in ticker_symbols:
-        df = data.xs(ticker, axis=1, level=1, drop_level=False).droplevel(1, axis=1)
-        df.to_csv(f'by_stock/{ticker}.csv')
-        print(f'Saved {ticker}.csv — shape: {df.shape}')
+    if isinstance(data.columns, pd.MultiIndex):
+        for ticker in ticker_symbols:
+            df = data.xs(ticker, axis=1, level=1, drop_level=False).droplevel(1, axis=1)
+            df.to_csv(f'by_stock/{ticker}.csv')
 
-# fetch and save SPY data
+    # fetch and save SPY data
 
-data = yf.download('SPY', period='max', interval='1d')
-df.to_csv(f'by_stock/SPY.csv')
+    data = yf.download('SPY', period='max', interval='1d')
+    df.to_csv(f'by_stock/SPY.csv')
+
+if __name__ == "__main__":
+    main()
